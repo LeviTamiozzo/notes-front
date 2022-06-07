f4d82d6f3b16e9f4f77e725e68103a44880feebf
Levi Tamiozzo <Levi_Tamiozzo@CORVEL.com>
2022-06-07 11:38:32 -0300

    Add env variables

--- src/config.js
+++ src/config.js
@@ -2,20 +2,20 @@ const config = {
     MAX_ATTACHMENT_SIZE: 5000000,
     // Backend config
     s3: {
-        REGION: 'us-east-1',
-        BUCKET: 'notes-api-prod-attachmentsbucket-w2mugfqw0dc8',
+        REGION: process.env.REACT_APP_REGION,
+        BUCKET: process.env.REACT_APP_BUCKET,
     },
     apiGateway: {
-        REGION: 'us-east-1',
-        URL: 'https://am3u5egud1.execute-api.us-east-1.amazonaws.com/prod',
+        REGION: process.env.REACT_APP_REGION,
+        URL: process.env.REACT_APP_API_URL,
     },
     cognito: {
-        REGION: 'us-east-1',
-        USER_POOL_ID: 'us-east-1_9YeIQQ0DF',
-        APP_CLIENT_ID: '6o4aner6be5kjodupb64a69vfg',
-        IDENTITY_POOL_ID: 'us-east-1:cc786932-b9e3-413c-b6a5-c1c9da5bc7a2',
+        REGION: process.env.REACT_APP_REGION,
+        USER_POOL_ID: process.env.REACT_APP_USER_POOL_ID,
+        APP_CLIENT_ID: process.env.REACT_APP_USER_POOL_CLIENT_ID,
+        IDENTITY_POOL_ID: process.env.REACT_APP_IDENTITY_POOL_ID,
     },
-    STRIPE_KEY: "pk_test_51L6fvqFltCheJNUe5EeTTr9CCh6EkpKszMEPaTw9BIRdYKIkFQkgpCnoxwgxX7TPbEZMvsPyPiiQsBRytLQXUaUk005RskVNxz",
+    STRIPE_KEY: process.env.STRIPE_KEY,
 };
 
 export default config;
